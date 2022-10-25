352790becce134169d33a59629086d362559bd70
Kaveh <aidivandi.dev@gmail.com>
2022-10-25 19:47:58 +0200

    MultiSigWallet Contract

--- contracts/MultiSigWallet.sol
+++ contracts/MultiSigWallet.sol
@@ -86,4 +86,47 @@ contract MultiSigWallet {
         approved[_txId][msg.sender] = true;
         emit Approve(msg.sender, _txId);
     }
+
+    function _getApprovalCount(uint _txId) internal view returns (uint count) {
+        for (uint i; i < owners.length; i++) {
+            if (approved[_txId][owners[i]]) {
+                count += 1;
+            }
+        }
+    }
+
+    function execute(uint _txId)
+        external
+        onlyOwner
+        txExists(_txId)
+        notExecuted(_txId)
+    {
+        require(
+            _getApprovalCount(_txId) >= required,
+            "tx not approved by required number of owners"
+        );
+
+        Transaction storage transaction = transactions[_txId];
+
+        transaction.executed = true;
+
+        (bool success, ) = transaction.to.call{value: transaction.value}(
+            transaction.data
+        );
+
+        require(success, "tx failed");
+        emit Execute(_txId);
+    }
+
+    function revoke(uint _txId)
+        external
+        onlyOwner
+        txExists(_txId)
+        notExecuted(_txId)
+    {
+        require(approved[_txId][msg.sender], "tx not approved");
+
+        approved[_txId][msg.sender] = false;
+        emit Revoke(msg.sender, _txId);
+    }
 }
